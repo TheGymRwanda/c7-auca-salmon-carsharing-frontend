605ae8eb4d255b4f16087c3c928549f15edc57cf
Babrah <usanasebabrah35@gmail.com>
2025-10-02 15:35:54 +0200

    feat(homepage): update layout and center buttons

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,8 +1,8 @@
 function HomePage() {
   return (
     <div className=" ">
-      <div className="flex flex-col justify-center md:justify-center font-serif font-bold">
-        <div className="flex flex-col items-center gap-1 mb-10">
+      <div className="flex flex-col justify-center font-serif font-bold md:justify-center">
+        <div className="mb-10 flex flex-col items-center gap-1">
           <h1 className="text-5xl font-bold">MONI</h1>
           <h1 className="text-5xl italic">share</h1>
         </div>
@@ -14,8 +14,8 @@ function HomePage() {
       </div>
 
       <div className="flex flex-col items-center md:justify-center  ">
-        <div className="  flex flex-col items-center gap-6 w-full max-w-xs ">
-          <button className="btn btn-primary  md:mt-0 mt-20">Book Car</button>
+        <div className="  flex w-full max-w-xs flex-col items-center gap-6 ">
+          <button className="btn btn-primary  mt-20 md:mt-0">Book Car</button>
           <p>or</p>
           <button className="btn btn-outline">See my Cars</button>
           <button className="btn btn-outline">See my Bookinngs</button>
