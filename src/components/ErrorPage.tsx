a2cb8503729130a5787ec36ecc37eafc465bd81e
Babrah <usanasebabrah35@gmail.com>
2025-10-02 21:40:17 +0200

    feat(ErrorPage): update layout and center buttons having the responsive

--- src/components/ErrorPage.tsx
+++ src/components/ErrorPage.tsx
@@ -3,8 +3,8 @@ import NotFoundIcon from '../assets/NotFoundIcon'
 function ErrorPage() {
   return (
     <div className="flex flex-col items-center justify-center gap-10 px-4 font-serif font-medium  text-center">
-      <div className="flex flex-col gap-4 md:gap-6 md:items-center">
-        <h1 className="text-5xl font-black md:text-6xl ">OOOOOPS!</h1>
+      <div className="flex flex-col md:items-center gap-4 md:gap-6 ">
+        <h1 className=" font-black text-5xl  md:text-6xl ">OOOOOPS!</h1>
         <NotFoundIcon className="ml-20 h-52 w-52 md:h-64 md:w-64 " />
       </div>
 
