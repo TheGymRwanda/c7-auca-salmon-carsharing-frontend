b016e808362a368deb0ac985b48f19eec7136489
kardara <abdoulayekardara@gmail.com>
2025-10-30 01:14:48 +0200

    fix: fixing types issues in manage booking

--- src/components/MyBookingsList.tsx
+++ src/components/MyBookingsList.tsx
@@ -15,30 +15,30 @@ export default function MyBookingsList({ img, carName, ownerName, index }: Booki
       </div>
 
       <div className="-mt-10 pl-5">
-        <h1 className="text-3xl font-serif">{carName}</h1>
+        <h1 className="font-serif text-3xl">{carName}</h1>
         <span className="text-md">Requested by {ownerName}</span>
       </div>
-      <div className="flex justify-between pl-5 gap-20">
-        <div className="flex flex-col gap-2 w-full">
+      <div className="flex justify-between gap-20 pl-5">
+        <div className="flex w-full flex-col gap-2">
           <span>from</span>
           <p className="flex gap-2">
             <DateIcon />
 
             <span>07 Jun 2023</span>
           </p>
-          <p className="flex gap-2 -ml-1">
+          <p className="-ml-1 flex gap-2">
             <TimeIcon />
             <span>13:09</span>
           </p>
         </div>
-        <div className="flex flex-col gap-2 w-full">
+        <div className="flex w-full flex-col gap-2">
           <span>to</span>
           <p className="flex gap-2">
             <DateIcon />
 
             <span>07 Jun 2023</span>
           </p>
-          <p className="flex gap-2 -ml-1">
+          <p className="-ml-1 flex gap-2">
             <TimeIcon />
 
             <span>14:09</span>
@@ -48,7 +48,7 @@ export default function MyBookingsList({ img, carName, ownerName, index }: Booki
       {index === 0 ? (
         <div>
           <p className="text-green-500">Booking accepted.</p>
-          <p className="w-3/5 text-accent-light mt-3">
+          <p className="mt-3 w-3/5 text-accent-light">
             You can not pick up the car before the agreed time
           </p>
         </div>
