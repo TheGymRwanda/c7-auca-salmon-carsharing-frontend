25c8b0bb294ac5e82b75f4b657fddf077f890291
andre-49 <andy49miguel@gmail.com>
2025-11-01 18:28:25 +0200

    fix: menus styled correctly

--- src/components/NavMenuList.tsx
+++ src/components/NavMenuList.tsx
@@ -11,42 +11,40 @@ import MenuOpt from './MenuOpt'
 
 export default function NavMenuList({ closeMenu }: { closeMenu?: () => void }) {
   return (
-    <>
-      <div className="space-y-1 p-4">
-        <MenuOpt to="/show-all-cars" closeMenu={closeMenu}>
-          <CarIcon className="size-5" />
-          <span className="text-sm font-medium">Show All Car</span>
-        </MenuOpt>
-        <MenuOpt to="/create-booking" closeMenu={closeMenu}>
-          <CarPlusIcon className="size-5" />
-          <span className="text-sm font-medium">Book A car</span>
-        </MenuOpt>
-        <MenuOpt to="/user-bookings" closeMenu={closeMenu}>
-          <CalendarIcon className="size-5" />
-          <span className="text-sm font-medium">My Bookings</span>
-        </MenuOpt>
-        <div className="my-4 border-t border-gray-100" />
-        <div className="px-4 py-2">
-          <h3 className="text-sm font-semibold text-white">My Cars</h3>
-        </div>
-        <MenuOpt to="/own-car" closeMenu={closeMenu}>
-          <CarsIcon className="size-5" />
-          <span className="text-sm font-medium">See My Cars</span>
-        </MenuOpt>
-        <MenuOpt to="/booking-management" closeMenu={closeMenu}>
-          <ListIcon className="size-5" />
-          <span className="text-sm font-medium">My Car&apos;s Bookings</span>
-        </MenuOpt>
-        <MenuOpt to="/add-car" closeMenu={closeMenu}>
-          <PlusIcon className="size-5" />
-          <span className="text-sm font-medium">Add New Car</span>
-        </MenuOpt>
-        <div className="my-4 border-t border-gray-100" />
-        <MenuOpt to="/" closeMenu={closeMenu} isLogout>
-          <LogoutIcon className="size-5" />
-          <span className="text-sm font-medium">Log Out</span>
-        </MenuOpt>
+    <div className="space-y-1 p-2">
+      <MenuOpt to="/show-all-cars" closeMenu={closeMenu}>
+        <CarIcon className="size-5" />
+        <span>Show All Car</span>
+      </MenuOpt>
+      <MenuOpt to="/create-booking" closeMenu={closeMenu}>
+        <CarPlusIcon className="size-5" />
+        <span>Book A car</span>
+      </MenuOpt>
+      <MenuOpt to="/user-bookings" closeMenu={closeMenu}>
+        <CalendarIcon className="size-5" />
+        <span>My Bookings</span>
+      </MenuOpt>
+      <div className="my-4 border-t border-gray-100" />
+      <div className="px-4 py-2">
+        <h3 className="text-sm font-semibold text-white">My Cars</h3>
       </div>
-    </>
+      <MenuOpt to="/own-car" closeMenu={closeMenu}>
+        <CarsIcon className="size-5" />
+        <span>See My Cars</span>
+      </MenuOpt>
+      <MenuOpt to="/booking-management" closeMenu={closeMenu}>
+        <ListIcon className="size-5" />
+        <span>My Car&apos;s Bookings</span>
+      </MenuOpt>
+      <MenuOpt to="/add-car" closeMenu={closeMenu}>
+        <PlusIcon className="size-5" />
+        <span>Add New Car</span>
+      </MenuOpt>
+      <div className="my-4 border-t border-gray-100" />
+      <MenuOpt to="/" closeMenu={closeMenu} isLogout>
+        <LogoutIcon className="size-5" />
+        <span>Log Out</span>
+      </MenuOpt>
+    </div>
   )
 }
--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -8,28 +8,27 @@ import MobileHamburger from '../components/MobileHamburger'
 import ProfileDropdown from '../components/ProfileDropdown'
 import NavMenuList from '../components/NavMenuList'
 import { AppRoutes } from '../routes/AppRoutes'
+import { ProfileIcon } from '../assets'
 
-export default function Navbar({
-  className,
-  profile = true,
-}: {
-  className?: string
-  profile?: boolean
-}): ReactElement {
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
+export default function Navbar(): ReactElement {
+  const [isNavMenuOpen, setIsNavMenuOpen] = useState(false)
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false)
   const location = useLocation()
   const hideMenuAndProfile =
     location.pathname === AppRoutes.landing || location.pathname === AppRoutes.login
   const token = localStorage.getItem('token')
-  const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
-  const closeMenu = () => setIsMenuOpen(false)
+  const toggleNavMenu = () => setIsNavMenuOpen(!isNavMenuOpen)
+  const toggleProfileMenu = () => setIsProfileMenuOpen(!isProfileMenuOpen)
+  const closeMenus = () => {
+    setIsNavMenuOpen(false)
+    setIsProfileMenuOpen(false)
+  }
 
   return (
     <>
       <nav
-        className={`fixed left-0 top-0 z-50 flex h-16 w-full items-center ${
-          !hideMenuAndProfile && profile ? 'justify-between' : 'justify-center'
-        } bg-slate-900 px-4 text-white shadow ${className}`}
+        className={`fixed left-0 top-0 z-50 flex h-16 w-full items-center justify-between
+         bg-slate-900 px-4 text-white shadow`}
       >
         <div
           className={`hidden items-center md:flex ${!token ? 'md:rounded-full md:border md:border-slate-900 md:bg-slate-900 md:p-7' : ''}`}
@@ -38,32 +37,41 @@ export default function Navbar({
         </div>
         {!hideMenuAndProfile && (
           <>
-            <MobileHamburger isMenuOpen={isMenuOpen} toggleMenu={toggleMenu} />
+            <MobileHamburger isMenuOpen={isNavMenuOpen} toggleMenu={toggleNavMenu} />
             <DesktopNav />
           </>
         )}
         <div className="flex flex-1 justify-center md:hidden">
           <LogoLink />
         </div>
-        {!hideMenuAndProfile && profile && (
-          <div className="flex items-center">
-            <ProfileDropdown />
-          </div>
+        {!hideMenuAndProfile && (
+          <button onClick={toggleProfileMenu} className="flex items-center">
+            <ProfileIcon className="size-6" />
+          </button>
         )}
       </nav>
-      {!hideMenuAndProfile && profile && (
+      {!hideMenuAndProfile && (
         <div
-          className={`fixed inset-0 z-50 transition-opacity duration-300 md:hidden ${
-            isMenuOpen ? 'pointer-events-auto opacity-100' : 'pointer-events-none opacity-0'
+          className={`fixed flex inset-0 z-50 transition-opacity duration-300 md:hidden ${
+            isNavMenuOpen || isProfileMenuOpen
+              ? 'pointer-events-auto opacity-100'
+              : 'pointer-events-none opacity-0'
           }`}
         >
-          <div className="absolute inset-0 bg-black/50" onClick={closeMenu} />
+          <div className="absolute inset-0 bg-black/50" onClick={closeMenus} />
           <div
-            className={`max-h-sm absolute left-5 top-20 w-80 max-w-[85vw] rounded-xl bg-background shadow-xl transition-transform duration-300 ${
-              isMenuOpen ? 'translate-x-0' : '-translate-x-full'
+            className={`max-h-sm absolute left-3 top-20 w-56 max-w-[85vw] rounded-xl bg-background shadow-xl transition-transform duration-300 ${
+              isNavMenuOpen ? 'translate-x-0' : '-translate-x-full'
             }`}
           >
-            <NavMenuList closeMenu={closeMenu} />
+            {isNavMenuOpen && <NavMenuList closeMenu={closeMenus} />}
+          </div>
+          <div
+            className={`max-h-sm absolute right-3 top-20 w-32 max-w-[85vw] rounded-xl bg-background shadow-xl transition-transform duration-300 ${
+              isProfileMenuOpen ? 'translate-x-0' : '-translate-x-full'
+            }`}
+          >
+            {isProfileMenuOpen && <ProfileDropdown closeMenu={closeMenus} />}
           </div>
         </div>
       )}
